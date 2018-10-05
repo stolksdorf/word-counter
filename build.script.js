d62d1261777911fe3c7618aee3835d748ef744aa
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2018-10-05 11:46:10 -0400

    Fixing some css bugs and asset pathing for prod

--- build.script.js
+++ build.script.js
@@ -6,13 +6,17 @@ String.prototype.replaceAll = function (find, replace) {
 	return str.replace(new RegExp(find, 'g'), replace);
 };
 
-//build().then(()=>{
-build(null, {prod : true}).then(()=>{
+build().then(()=>{
+//build(null, {prod : true}).then(()=>{
 	const render = require('./docs/main/render.js');
 
 	fs.writeFileSync('./docs/index.html', render()
 		.replace('/libs.js', 'libs.js')
+		.replace('.hydrate(', '.render(')
 		.replaceAll('/main/bundle', 'main/bundle')
+		.replaceAll('/assets', 'assets')
+	);
+	fs.writeFileSync('./docs/main/bundle.css', fs.readFileSync('./docs/main/bundle.css', 'utf8')
 		.replaceAll('/assets', 'assets')
 	);
 	console.log('Built index.html');
--- client/main/default.text.js
+++ client/main/default.text.js
@@ -1,1 +1,20 @@
-module.exports = `this is the default text`;
+module.exports = `Type in here to get a live word count.
+
+It's smart, so symbol separated words get counted properly
+
+people+cheating-by|using_symbols=to*split#words is 7 words
+
+
+But ASCII art or tables gets only counted by word
+
+| d4 | Result |
+---------------
+| 1  | Sock   |
+| 2  | Shoe   |
+| 3  | Pants  |
+| 4  | Hat    |
+
+is only 10 words
+
+
+`;
